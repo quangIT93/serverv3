ca0ea59198737d819ee7fbdaeb40c39e87163e8b
Phan Công Thăng <57101433+phanthang-creat@users.noreply.github.com>
2023-07-04 13:23:03 +0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
     app.enableCors({
       origin: '*',
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-      allowedHeaders: 'Content-Type, Accept',
+      // allowedHeaders: 'Content-Type, Accept',
     });
 
   const openAPIConfig = new DocumentBuilder()
