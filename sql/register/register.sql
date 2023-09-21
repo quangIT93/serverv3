d2b025e56d23eb11b3841e07702d121cab6e696a
ToanHuynh1 <104150290+ToanHuynh1@users.noreply.github.com>
2023-09-21 11:34:08 +0700

    update

---
+++ sql/register/register.sql
@@ -0,0 +1,15 @@
+
+
+ALTER TABLE accounts ADD COLUMN `name` varchar(200) DEFAULT NULL;
+
+
+ALTER TABLE accounts ADD COLUMN `password` varchar(255) DEFAULT NULL;
+
+
+
+
+ALTER TABLE otps ADD COLUMN `name` varchar(200) DEFAULT NULL;
+
+
+ALTER TABLE otps ADD COLUMN `password` varchar(255) DEFAULT NULL;
+
