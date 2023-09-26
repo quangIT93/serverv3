8a9291bd4b95d827a3474f37f9ff96c6b01e5b7b
ToanHuynh1 <104150290+ToanHuynh1@users.noreply.github.com>
2023-09-26 08:38:25 +0700

    update

--- src/models/academic_types/academic_types.module.ts
+++ src/models/academic_types/academic_types.module.ts
@@ -3,10 +3,12 @@ import { AcademicTypesService } from './academic_types.service';
 import { AcademicTypesController } from './academic_types.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AcademicType } from './entities/academic_type.entity';
+import { JwtAccessTokenServiceModule } from 'src/providers/jwt/atk.provider.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([AcademicType])
+    TypeOrmModule.forFeature([AcademicType]),
+    JwtAccessTokenServiceModule
   ],
   controllers: [AcademicTypesController],
   providers: [AcademicTypesService],
