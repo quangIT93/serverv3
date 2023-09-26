4f3677125564ddbc46f5aab4c3a4f531e98838e9
ToanHuynh1 <104150290+ToanHuynh1@users.noreply.github.com>
2023-09-26 09:39:44 +0700

    update

--- src/models/cv-filter/cv-filter.controller.ts
+++ src/models/cv-filter/cv-filter.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Query, Req, UseGuards } from '@nestjs/common';
 import { CvFilterService } from './cv-filter.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { FilterCandidatesDto } from './dto/filter-candidates.dto';
+import { AuthGuard } from 'src/authentication/auth.guard';
+import { CustomRequest } from 'src/common/interfaces/customRequest.interface';
 
 @Controller('cv-filter')
 @ApiTags('CV filter')
@@ -9,7 +11,23 @@ export class CvFilterController {
   constructor(private readonly cvFilterService: CvFilterService) {}
 
   @Get('search')
-  filterCandidatesWithCondition(@Query() query: FilterCandidatesDto) {
-    return this.cvFilterService.filterCandidatesWithCondition(query);
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  async filterCandidatesWithCondition(@Query() query: FilterCandidatesDto, @Req() req: CustomRequest) {
+    try {
+
+      const accountId = req.user?.id
+
+      if (!accountId) {
+        throw new BadRequestException('User not found')
+      }
+      return await this.cvFilterService.filterCandidatesWithCondition(query)
+
+    } catch (error) {
+     if (error instanceof Error) {
+      throw new BadRequestException(error.message)
+     }
+     throw new BadRequestException('Error getting')
+    }
   }
 }
