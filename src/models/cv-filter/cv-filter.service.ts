4c7d87d3f34e8ae02fb6665025c13f9d294a2da5
ToanHuynh1 <104150290+ToanHuynh1@users.noreply.github.com>
2023-09-26 11:02:26 +0700

    fix bug

--- src/models/cv-filter/cv-filter.service.ts
+++ src/models/cv-filter/cv-filter.service.ts
@@ -25,27 +25,33 @@ export class CvFilterService {
       } = query;
 
       const candidates = this.profileRepository
-        .createQueryBuilder('profile')
-        .leftJoinAndSelect('profile.childCategories', 'childCategory')
-        .leftJoinAndSelect('profile.profilesEducations', 'profilesEducations');
+        .createQueryBuilder('profiles')
+        .leftJoinAndSelect('profiles.childCategories', 'childCategory')
+        .leftJoinAndSelect('profiles.profilesEducations', 'profilesEducations');
 
-      if (addresses && addresses.length > 0) {
-        candidates.andWhere('profile.address IN (:...addresses)', {
-          addresses,
+      if (addresses) {
+        candidates.andWhere('profiles.address IN (:...addresses)', {
+          addresses: Array.isArray(addresses)
+            ? addresses.map((item) => +item)
+            : [+addresses],
         });
       }
 
-      if (categories && categories.length > 0) {
+      if (categories) {
         candidates.andWhere('childCategory.id IN (:...categories)', {
-          categories,
+          categories: Array.isArray(categories)
+            ? categories.map((item) => +item)
+            : [+categories],
         });
       }
 
-      if (educations && educations.length > 0) {
+      if (educations) {
         candidates.andWhere(
           'profilesEducations.academicTypeId IN (:...educations)',
           {
-            educations: educations.map((i) => +i),
+            educations: Array.isArray(educations)
+              ? educations.map((item) => +item)
+              : [+educations],
           },
         );
       }
@@ -53,7 +59,7 @@ export class CvFilterService {
       if (ageMin) {
         const yearMIn = new Date().getFullYear() - ageMin;
         const birthdayMin = +new Date(yearMIn + 1, 1, 1);
-        candidates.andWhere('profile.birthday <= :birthdayMin', {
+        candidates.andWhere('profiles.birthday <= :birthdayMin', {
           birthdayMin,
         });
       }
@@ -61,25 +67,22 @@ export class CvFilterService {
       if (ageMax) {
         const yearMax = +new Date().getFullYear() - ageMax;
         const birthdayMax = +new Date(yearMax, 1, 1);
-        candidates.andWhere('profile.birthday >= :birthdayMax', {
+        candidates.andWhere('profiles.birthday >= :birthdayMax', {
           birthdayMax,
         });
       }
 
       if (gender) {
-        candidates.andWhere('profile.gender = :gender', {
+        candidates.andWhere('profiles.gender = :gender', {
           gender,
         });
       }
 
-      const data = await candidates
-        .where({ isSearch: 1 })
+      return await candidates
+        .andWhere({ isSearch: 0 })
         .take(limit)
         .skip(page * limit)
         .getMany();
-
-      console.log(data);
-      return data;
     } catch (error) {
       if (error instanceof QueryFailedError) {
         throw new InternalServerErrorException();
