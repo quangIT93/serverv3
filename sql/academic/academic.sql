f59cbac9dfb8987d016bb1cc5caead6b07c1e18e
NguyenTien99 <nguyenmtien0@gmail.com>
2023-09-27 15:48:32 +0700

    'InsertAcademicType'

--- sql/academic/academic.sql
+++ sql/academic/academic.sql
@@ -12,4 +12,14 @@ ALTER TABLE `profiles_educations` ADD COLUMN `academic_type_id` TINYINT(4) DEFAU
 
 ALTER TABLE `profiles_educations` ADD CONSTRAINT FK_AcademicTypes_ProfilesEducations FOREIGN KEY (academic_type_id) REFERENCES academic_types(id) ON DELETE CASCADE ON UPDATE NO ACTION;
 
+INSERT INTO `academic_types` (value,value_en,value_ko,status) VALUES ('Cấp 1','Primary school','초등학교',1);
+INSERT INTO `academic_types` (value,value_en,value_ko,status) VALUES ('Cấp 2','Secondary school','중학교',1);
+INSERT INTO `academic_types` (value,value_en,value_ko,status) VALUES ('Cấp 3','High school','고등학교',1);
+INSERT INTO `academic_types` (value,value_en,value_ko,status) VALUES ('Trung cấp/ nghề','Trade school','전문학교',1);
+INSERT INTO `academic_types` (value,value_en,value_ko,status) VALUES ('Cao đẳng','Community College','전문 대학',1);
+INSERT INTO `academic_types` (value,value_en,value_ko,status) VALUES ('Đại học','University','대학교',1);
+INSERT INTO `academic_types` (value,value_en,value_ko,status) VALUES ('Sau đại học (Thạc sĩ, Tiến sĩ)',"After university (Master's degree, Doctoral degree)",'대학 졸업 후 (석사, 박사)',1);
+INSERT INTO `academic_types` (value,value_en,value_ko,status) VALUES ('Khác','Other','다른',1);
+
+
 
