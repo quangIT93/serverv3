3c47b6e20c055c2df2fc0683a49e10303b8dab33
ToanHuynh1 <104150290+ToanHuynh1@users.noreply.github.com>
2023-10-11 11:01:47 +0700

    modify total

--- src/models/view_profiles/view_profiles.service.ts
+++ src/models/view_profiles/view_profiles.service.ts
@@ -13,41 +13,38 @@ export class ViewProfilesService {
     private readonly userService: UserService,
   ) {}
   async create(createViewProfileDto: CreateViewProfileDto) {
-    const TOTAL_IN_DAY = 3;
-
     const user = await this.userService.findRoleById(
       createViewProfileDto.recruitId,
     );
+
+    const TOTAL_IN_DAY = (user && (user?.role === 3 || user?.role === 1)) ? 10 : 3;
 
     if (user?.type !== 1) {
       throw new BadRequestException('Is not cruitment');
     }
 
-    if (user?.role !== 1 && user?.role !== 3) {
-      const result = await this.viewProfileRepository
-        .createQueryBuilder('view_profiles')
-        .where('view_profiles.recruitId = :recruitId', {
-          recruitId: createViewProfileDto.recruitId,
-        })
-        .andWhere('DATE(view_profiles.created_at) = :today', {
-          today: new Date().toISOString().split('T')[0],
-        })
-        .getCount();
+    const result = await this.viewProfileRepository
+      .createQueryBuilder('view_profiles')
+      .where('view_profiles.recruitId = :recruitId', {
+        recruitId: createViewProfileDto.recruitId,
+      })
+      .andWhere('DATE(view_profiles.created_at) = :today', {
+        today: new Date().toISOString().split('T')[0],
+      })
+      .getCount();
 
-      if (result >= TOTAL_IN_DAY) {
-        return 0;
-      }
-
-      const newEntity = this.viewProfileRepository.create(createViewProfileDto);
-
-      const data = await this.viewProfileRepository.save(newEntity);
-
-      if (data) {
-        return TOTAL_IN_DAY - (result + 1);
-      }
-
-      return TOTAL_IN_DAY - result;
+    if (result >= TOTAL_IN_DAY) {
+      return 0;
     }
-    return TOTAL_IN_DAY;
+
+    const newEntity = this.viewProfileRepository.create(createViewProfileDto);
+
+    const data = await this.viewProfileRepository.save(newEntity);
+
+    if (data) {
+      return TOTAL_IN_DAY - (result + 1);
+    }
+
+    return TOTAL_IN_DAY - result;
   }
 }
