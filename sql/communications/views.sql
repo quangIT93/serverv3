79f2ab1b947e1c393893e280109f71a77350eab5
Phan Công Thăng <57101433+phanthang-creat@users.noreply.github.com>
2023-10-12 16:16:56 +0700

    disable auth controller

--- sql/communications/views.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `communication_views`;
-
-CREATE TABLE `communication_views` (
-    communication_id int(11) NOT NULL,
-    account_id varchar(50) NOT NULL COMMENT 'account_id or ip address',
-    created_at datetime DEFAULT current_timestamp(),
-    PRIMARY KEY (communication_id, account_id),
-    Constraint FK_Communications_CommunicationViews FOREIGN KEY (communication_id) REFERENCES communications (id) ON DELETE CASCADE ON UPDATE NO ACTION
-)
