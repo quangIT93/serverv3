8edcacf0eaed1ce187ebef4a7a04f133d73afb4d
Phan Công Thăng <57101433+phanthang-creat@users.noreply.github.com>
2023-10-20 16:13:36 +0700

    Update hot-topics.service.ts

--- src/models/hot-topics/hot-topics.service.ts
+++ src/models/hot-topics/hot-topics.service.ts
@@ -23,14 +23,14 @@ export class HotTopicsService {
             .andWhere(`${dto.version === 'app' ? 'hot_topics.image IS NOT NULL' : '1=1'}`)
             .select([
                 'hot_topics.id',
-                'hot_topics.type',
-                'hot_topics.detailId',
+                // 'hot_topics.type',
+                // 'hot_topics.detailId',
                 'hot_topics.title',
                 `${version === 1 ? 'hot_topics.image' : 'hot_topics.webImage'}`,
                 'hot_topics.themeId',
-                'hot_topics.order',
-                'hot_topics.status',
-                'hot_topics.createdAt',
+                // 'hot_topics.order',
+                // 'hot_topics.status',
+                // 'hot_topics.createdAt',
                 'hot_topics.query',
             ])
             .orderBy('hot_topics.order', 'ASC')
