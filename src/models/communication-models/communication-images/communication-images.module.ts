ee7dd1516a0e2dde0ad28921d98222022ce3c226
NguyenTien99 <nguyenmtien0@gmail.com>
2023-12-11 10:06:49 +0700

    add api upload image communication

--- src/models/communication-models/communication-images/communication-images.module.ts
+++ src/models/communication-models/communication-images/communication-images.module.ts
@@ -4,16 +4,16 @@ import { CommunicationImagesController } from './communication-images.controller
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommunicationImage } from './entities/communication-image.entity';
 import { JwtAccessTokenServiceModule } from 'src/providers/jwt/atk.provider.module';
+import { AWSModule } from 'src/providers/storage/aws/provider.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([
-      CommunicationImage
-    ]),
-    JwtAccessTokenServiceModule
+    TypeOrmModule.forFeature([CommunicationImage]),
+    JwtAccessTokenServiceModule,
+    AWSModule,
   ],
   controllers: [CommunicationImagesController],
   providers: [CommunicationImagesService],
-  exports: [CommunicationImagesService]
+  exports: [CommunicationImagesService],
 })
 export class CommunicationImagesModule {}
