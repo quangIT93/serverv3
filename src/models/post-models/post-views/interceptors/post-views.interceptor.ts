49fef6723cc204376e11efd423d6ee6b173e8a62
NguyenTien99 <nguyenmtien0@gmail.com>
2023-12-20 11:02:13 +0700

    update 3 api log companny

--- src/models/post-models/post-views/interceptors/post-views.interceptor.ts
+++ src/models/post-models/post-views/interceptors/post-views.interceptor.ts
@@ -1,54 +1,55 @@
 import {
-    CallHandler,
-    ExecutionContext,
-    Injectable,
-    NestInterceptor,
-  } from '@nestjs/common';
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
 import { Observable, map } from 'rxjs';
 import { PostView } from '../entities/post-view.entity';
 import { PostNormally } from '../../posts/serialization/normally-post.class';
-  @Injectable()
-  export class PostViewInterceptor implements NestInterceptor {
-    async intercept(
-      _context: ExecutionContext,
-      next: CallHandler<any>,
-    ): Promise<Observable<any>> {
-      const lang = _context.switchToHttp().getRequest()['lang'];
+@Injectable()
+export class PostViewInterceptor implements NestInterceptor {
+  async intercept(
+    _context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Promise<Observable<any>> {
+    const lang = _context.switchToHttp().getRequest()['lang'];
 
-      return next.handle().pipe(
-        map((posts: PostView[]) => {
-          const length = posts.length;
-          let isOver = true;
-  
-          if (length === _context.switchToHttp().getRequest().limit) {
-            isOver = false;
-            posts.pop();
-          }
-  
-          if (length === 0) {
-            return {
-              status: _context.switchToHttp().getResponse().statusCode,
-              data: [],
-              message: _context.switchToHttp().getResponse().statusMessage,
-              is_over: true,
-            };
-          }
-          const data = posts.map((post: PostView) => {
-            const postNormally = new PostNormally(post.post, lang);
-  
+    return next.handle().pipe(
+      map((posts: PostView[]) => {
+        const length = posts.length;
+        let isOver = true;
 
-            postNormally.bookmarked = post.post.bookmarks ? post.post.bookmarks.length > 0 : false;
-            return postNormally;
-          });
-  
+        if (length === _context.switchToHttp().getRequest().limit) {
+          isOver = false;
+          posts.pop();
+        }
+
+        if (length === 0) {
           return {
             status: _context.switchToHttp().getResponse().statusCode,
+            data: [],
             message: _context.switchToHttp().getResponse().statusMessage,
-            is_over: isOver,
-            data,
+            is_over: true,
           };
-        }),
-      );
-    }
+        }
+        const data = posts.map((post: PostView) => {
+          const postNormally = new PostNormally(post.post, lang);
+
+          postNormally.bookmarked = post.post.bookmarks
+            ? post.post.bookmarks.length > 0
+            : false;
+          postNormally['created_at'] = new Date(post.createdAt).getTime();
+          return postNormally;
+        });
+
+        return {
+          status: _context.switchToHttp().getResponse().statusCode,
+          message: _context.switchToHttp().getResponse().statusMessage,
+          is_over: isOver,
+          data,
+        };
+      }),
+    );
   }
-  
+}
